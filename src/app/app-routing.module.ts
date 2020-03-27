8af4b8c4555c828d03f70e9cb4b6ef0d0ad5d44d
Kerem Çakırer <kcakirer@hotmail.com>
2020-03-27 23:36:52 +0300

    Add routing for game component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { GameComponent } from './game/game.component';
 import { HomeComponent } from './home/home.component';
 import { SessionComponent } from './session/session.component';
 
 // TODO: Handle incorrect paths too
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
-  { path: 'sessions/:sid', component: SessionComponent }
+  { path: 'sessions/:sid', component: SessionComponent },
+  { path: 'sessions/:sid/games/:gid', component: GameComponent }
 ];
 
 @NgModule({
