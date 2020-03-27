767c55eab62c50db022f08b035b793628c432117
cherrytoska <budanaz.yakup@gmail.com>
2020-03-27 20:27:59 +0100

    some wrappers

--- src/app/chessboard/chessboard.component.ts
+++ src/app/chessboard/chessboard.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-chessboard',
-  templateUrl: './chessboard.component.html',
-  styleUrls: ['./chessboard.component.css']
+  selector: "app-chessboard",
+  templateUrl: "./chessboard.component.html",
+  styleUrls: ["./chessboard.component.css"]
 })
 export class ChessboardComponent implements OnInit {
+  @Input() board1?: boolean;
 
-  constructor() { }
+  board?: Chessboard;
+  constructor() {}
 
   ngOnInit(): void {
+    this.board = Chessboard("board1", {
+      draggable: true,
+      dropOffBoard: "trash",
+      sparePieces: true,
+      orientation: "white",
+      onDrop: this.revDrop.bind(this),
+      position: "start",
+      pieceTheme: "../../assets/img/chesspieces/wikipedia/{piece}.png"
+    });
   }
 
+
+  revDrop(
+    source: string,
+    target: string,
+    piece: string,
+    newPos: ChessboardPosition,
+    oldPos: ChessboardPosition,
+    orientation: ChessboardOrientation
+  ): any {
+    //change this with response
+    if (true) {
+      let st = ''.concat(target, '-', source);
+      console.log(st);
+      let x = this.board?.move(st);
+      console.log(x);
+      console.log("uwu");
+      return;
+      //return "snapback";
+    } else {
+      return;
+    }
+  }
 }
+
+function preventDrop(event: DragEvent): void {
+  if (typeof event.cancelable !== "boolean" || event.cancelable) {
+    // The event can be canceled, so we do so.
+    event.preventDefault();
+  } else {
+    // The event cannot be canceled, so it is not safe
+    // to call preventDefault() on it.
+    console.warn(`The following event couldn't be canceled:`);
+    console.dir(event);
+  }
+}
+
+function listenOnDragMove(): void {}
+
+function listenOnChange(): void {}
