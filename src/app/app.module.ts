f9f77e2ae101330a4f69647836b7056b15ce1ce9
Kerem Çakırer <kcakirer@hotmail.com>
2020-04-01 17:00:11 +0300

    Purge Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -30,9 +28,7 @@ import { SessionComponent } from './session/session.component';
     HttpClientModule,
     FormsModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
     FontAwesomeModule,
-    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
