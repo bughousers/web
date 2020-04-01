9a6297d3a28c4a5fdf016c0836cb73b37d1e6179
Kerem Çakırer <kcakirer@hotmail.com>
2020-04-01 15:07:52 +0300

    Add functions for managing cookies

---
+++ src/app/cookies.ts
@@ -0,0 +1,17 @@
+export function getCookie(name: string): string | undefined {
+  const cookies = document.cookie
+    .split(';')
+    .map(s => s.trimLeft())
+    .map(s => s.split('=', 2))
+    .filter(c => c.length === 2);
+  for (const c of cookies) {
+    if (c[0] === name) {
+      return c[1];
+    }
+  }
+  return undefined;
+}
+
+export function setCookie(name: string, value: string) {
+  document.cookie = `${name}=${value}`;
+}
