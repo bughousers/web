47dbdb48cc8b5166a913b665e4a02bf433d76ec6
Kerem Çakırer <kcakirer@hotmail.com>
2020-04-17 23:08:06 +0300

    Switch from Promises to Observables

--- src/app/networking.service.ts
+++ src/app/networking.service.ts
@@ -1,7 +1,7 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {fromEvent, Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {ignoreElements, map} from 'rxjs/operators';
 
 import {environment} from '../environments/environment';
 import {Connected, Created, Event, Joined} from './networking';
@@ -21,27 +21,25 @@ export class NetworkingService {
   constructor(private http: HttpClient) {
   }
 
-  createSession(ownerName: string): Promise<Created> {
+  create(ownerName: string): Observable<Created> {
     return this.http.post<Created>(`${environment.apiUrl}/v1/sessions`, JSON.stringify({
       ownerName
-    })).toPromise();
+    }));
   }
 
-  joinSession(sessionId: string, userName: string): Promise<Joined> {
-    return this.http.post<Joined>(
-      `${environment.apiUrl}/v1/sessions/${sessionId}`,
-      JSON.stringify({userName})
-    ).toPromise();
+  join(sessionId: string, userName: string): Observable<Joined> {
+    return this.http.post<Joined>(`${environment.apiUrl}/v1/sessions/${sessionId}`, JSON.stringify({
+      userName
+    }));
   }
 
-  connectSession(settings: Settings): Promise<Connected> {
-    return this.http.post<Connected>(
-      `${environment.apiUrl}/v1/sessions/${settings.sessionId}`,
+  connect(settings: Settings): Observable<Connected> {
+    return this.http.post<Connected>(`${environment.apiUrl}/v1/sessions/${settings.sessionId}`,
       JSON.stringify({authToken: settings.authToken})
-    ).toPromise();
+    );
   }
 
-  subscribe(settings: Settings): Observable<Event> {
+  sse(settings: Settings): Observable<Event> {
     let eventSource = this.eventSources.get(settings.sessionId);
     if (eventSource === undefined) {
       eventSource = new EventSource(`${environment.apiUrl}/v1/sessions/${settings.sessionId}/sse`);
@@ -50,44 +48,40 @@ export class NetworkingService {
     return fromEvent<MessageEvent>(eventSource, 'message').pipe(map(ev => JSON.parse(ev.data)));
   }
 
-  async changeParticipants(settings: Settings, participants: number[]): Promise<void> {
-    await this.http.post(
-      `${environment.apiUrl}/v1/sessions/${settings.sessionId}/participants`,
+  changeParticipants(settings: Settings, participants: number[]): Observable<never> {
+    return this.http.post(`${environment.apiUrl}/v1/sessions/${settings.sessionId}/participants`,
       JSON.stringify({
         authToken: settings.authToken,
         participants
       }),
       {responseType: 'text'}
-    ).toPromise();
+    ).pipe(ignoreElements());
   }
 
-  async deployPiece(settings: Settings, piece: string, pos: string): Promise<void> {
-    await this.http.post(
-      `${environment.apiUrl}/v1/sessions/${settings.sessionId}/games/_/board`,
+  deployPiece(settings: Settings, piece: string, pos: string): Observable<never> {
+    return this.http.post(`${environment.apiUrl}/v1/sessions/${settings.sessionId}/games/_/board`,
       JSON.stringify({
         type: 'deploy',
         authToken: settings.authToken,
         piece, pos
       }),
       {responseType: 'text'}
-    ).toPromise();
+    ).pipe(ignoreElements());
   }
 
-  async movePiece(settings: Settings, move: string): Promise<void> {
-    await this.http.post(
-      `${environment.apiUrl}/v1/sessions/${settings.sessionId}/games/_/board`,
+  movePiece(settings: Settings, move: string): Observable<never> {
+    return this.http.post(`${environment.apiUrl}/v1/sessions/${settings.sessionId}/games/_/board`,
       JSON.stringify({
         type: 'move',
         authToken: settings.authToken,
         change: move
       }),
       {responseType: 'text'}
-    ).toPromise();
+    ).pipe(ignoreElements());
   }
 
-  async promotePiece(settings: Settings, move: string, upgradeTo: string): Promise<void> {
-    await this.http.post(
-      `${environment.apiUrl}/v1/sessions/${settings.sessionId}/games/_/board`,
+  promotePiece(settings: Settings, move: string, upgradeTo: string): Observable<never> {
+    return this.http.post(`${environment.apiUrl}/v1/sessions/${settings.sessionId}/games/_/board`,
       JSON.stringify({
         type: 'promote',
         authToken: settings.authToken,
@@ -95,7 +89,7 @@ export class NetworkingService {
         upgradeTo
       }),
       {responseType: 'text'}
-    ).toPromise();
+    ).pipe(ignoreElements());
   }
 
 }
